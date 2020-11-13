7e3bf4b40e4345b2c515cca5935bc26e35a9cf72
Adriano Costa <adrianocostaads@gmail.com>
2020-11-13 14:46:27 -0300

    SellerForm

---
+++ src/gui/SellerFormController.java
@@ -0,0 +1,159 @@
+package gui;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+import db.DbException;
+import gui.listeners.DataChangeListener;
+import gui.util.Alerts;
+import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import model.entities.Seller;
+import model.exceptions.ValidationException;
+import model.services.SellerService;
+
+public class SellerFormController implements Initializable {
+
+	private Seller entity;
+	
+	private SellerService service;
+	
+	//vai poder permitir outros objetos se inscreverem nessa lista e receberem o evento.
+	private List <DataChangeListener> datachangelistener = new ArrayList<>();
+	
+	@FXML
+	private TextField txtId;
+	
+	@FXML
+	private TextField txtName;
+	
+	@FXML
+	private Label labelErrorName;
+	
+	@FXML
+	private Button btSave;
+	
+	@FXML
+	private Button btCancel;
+	
+	public void setSeller(Seller entity) {
+		this.entity = entity;
+	}
+	
+	
+	public void setSellerService(SellerService service) {
+		this.service = service;
+	}
+	
+	//esse metodo vai escrever esse listener na lista.
+	
+	public void subscribeDataChangeListener(DataChangeListener listener) {
+		datachangelistener.add(listener);
+	}
+	
+	//esse getformdata vai pegar os dados das caixinhas do formulario e instanciar um departamento.
+	@FXML
+	public void onBtSaveAction(ActionEvent event) {
+		if(entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		if(service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		try {
+			entity = getFormData();
+			service.saveOrUpdate(entity);
+			notifyDataChangeListener();
+			//pegar a referencia da janela e fechar a janela
+			Utils.currentStage(event).close();
+		}
+		
+		catch(ValidationException e) {
+			//vai ser uma coleção de erros.
+			setErrorMessages(e.getErrors());
+		}
+		
+	catch(DbException e) {
+		Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
+	}
+	}
+	
+	//vai executar o metodo ondatachanged da interface datachangelistener
+	//vai emitir o evento ondatachanged para cada listener.
+	private void notifyDataChangeListener() {
+		//para cada datachangelistener pertencente a lista datachangerlisteners
+		for(DataChangeListener listener : datachangelistener) {
+			listener.OnDataChanged();
+		}
+		
+		
+	}
+
+
+	private Seller getFormData() {
+		Seller obj = new Seller();
+		ValidationException exception = new ValidationException("Validation error");
+		
+		
+		obj.setId(Utils.tryParseToInt( txtId.getText()));
+		
+		if(txtName.getText() == null || txtName.getText().trim().equals("")) {
+			exception.addError("name", "Field can´t be empty");
+		}
+		obj.setName(txtName.getText());
+		
+		if(exception.getErrors().size()>0) {
+			throw exception;
+		}
+		
+		return obj;
+	}
+
+
+	@FXML
+	public void onBtCancelAction(ActionEvent event) {
+		//botao de cancelar fecha a janela.
+		Utils.currentStage(event).close();
+	}
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+		
+	}
+
+	private void initializeNodes() {
+		Constraints.setTextFieldInteger(txtId);
+		Constraints.setTextFieldMaxLength(txtName, 30);
+	}
+	
+	//jogar nas caixas de texto os dados que estão no objeto entity la de cima.
+	public void updateFormData() {
+		//verificação para testar se o entity é null
+		if(entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		//a caixa de texto trabalha com string - entao converte int para string.
+		txtId.setText(String.valueOf(entity.getId()));
+		txtName.setText(entity.getName());
+	}
+	//essa coleção vai carregar os erros, percorrer essa coleção, preenchendo os erros nas caixas de texto.
+	private void setErrorMessages(Map<String, String> errors) {
+		Set<String> fields = errors.keySet();
+		if(fields.contains("name")) {
+			labelErrorName.setText(errors.get("name"));
+		}
+	}
+	
+}
--- src/gui/SellerListController.java
+++ src/gui/SellerListController.java
@@ -1,5 +1,6 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.Date;
 import java.util.List;
@@ -16,7 +17,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
@@ -24,6 +27,8 @@ import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Seller;
 import model.services.SellerService;
@@ -119,8 +124,8 @@ public class SellerListController implements Initializable, DataChangeListener {
 		initRemoveButtons();
 	}
 	//vai receber por parametro uma referencia para o stage da janela que criou a janela de dialogo
-//	private void createDialogForm(Seller obj, String absoluteName, Stage parentStage) {
-	/*	try {
+	private void createDialogForm(Seller obj, String absoluteName, Stage parentStage) {
+		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
 			Pane pane = loader.load(); 
 			
@@ -160,7 +165,7 @@ public class SellerListController implements Initializable, DataChangeListener {
 		catch(IOException e) {
 			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
 		}
-	} */
+	} 
 
 	//o que vai ter que executar no departmentlistcontroller quando receber uma notificação que os dados foram atualizados
 	//função que faz essa atualização : updatetableview.
@@ -184,8 +189,8 @@ public class SellerListController implements Initializable, DataChangeListener {
 					return;
 				}
 				setGraphic(button);
-			//	button.setOnAction(
-			//			event -> createDialogForm(obj, "/gui/SellerForm.fxml", Utils.currentStage(event)));
+				button.setOnAction(
+					event -> createDialogForm(obj, "/gui/SellerForm.fxml", Utils.currentStage(event)));
 			}
 		});
 	}
