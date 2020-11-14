7e39bf2bdf9f5828972a995dbda9736d93704828
Adriano Costa <adrianocostaads@gmail.com>
2020-11-14 15:10:06 -0300

    Saving Seller

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -1,9 +1,11 @@
 package gui;
 
 import java.net.URL;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -144,13 +146,40 @@ public class SellerFormController implements Initializable {
 		Seller obj = new Seller();
 		ValidationException exception = new ValidationException("Validation error");
 		
-		
+		//jogando o conteudo da caixinha do id para o campo id do objeto.
 		obj.setId(Utils.tryParseToInt( txtId.getText()));
 		
+		//testando se é igual a nulo ou string vazio.
 		if(txtName.getText() == null || txtName.getText().trim().equals("")) {
 			exception.addError("name", "Field can´t be empty");
 		}
 		obj.setName(txtName.getText());
+		
+		if(txtEmail.getText() == null || txtEmail.getText().trim().equals("")) {
+			exception.addError("email", "Field can´t be empty");
+		}
+		obj.setName(txtEmail.getText());
+		
+		//startofday vai converter a data que foi escolhida no pc do usuario para o instant que é uma data independente de localidade.
+		//assim que pega o valor do datepicker.
+		if(dpBirthDate.getValue() == null) {
+			exception.addError("birthDate", "Field can´t be empty");
+		}
+		else {
+			Instant instant = Instant.from(dpBirthDate.getValue().atStartOfDay(ZoneId.systemDefault()));
+			//converter instant para o date.
+			obj.setBirthDate(Date.from(instant));
+		}
+		
+		
+		if(txtBaseSalary.getText() == null || txtBaseSalary.getText().trim().equals("")) {
+			exception.addError("baseSalary", "Field can´t be empty");
+		}
+		
+		obj.setBaseSalary(Utils.tryParseToDouble(txtBaseSalary.getText()));
+		
+		obj.setDepartment(comboBoxDepartment.getValue());
+		
 		
 		if(exception.getErrors().size()>0) {
 			throw exception;
@@ -225,9 +254,16 @@ public class SellerFormController implements Initializable {
 	//essa coleção vai carregar os erros, percorrer essa coleção, preenchendo os erros nas caixas de texto.
 	private void setErrorMessages(Map<String, String> errors) {
 		Set<String> fields = errors.keySet();
-		if(fields.contains("name")) {
-			labelErrorName.setText(errors.get("name"));
-		}
+		
+		//se não houver erro  o label fica com texto vazio.
+		labelErrorName.setText(fields.contains("name") ? errors.get("name") : "");
+		
+		labelErrorEmail.setText(fields.contains("email") ? errors.get("email") : "");
+		labelErrorBirthDate.setText(fields.contains("birthDate") ? errors.get("birthDate") : "");
+		labelErrorBaseSalary.setText(fields.contains("baseSalary") ? errors.get("baseSalary") : "");
+		
+		
+		
 	}
 	
 	private void initializeComboBoxDepartment() {
